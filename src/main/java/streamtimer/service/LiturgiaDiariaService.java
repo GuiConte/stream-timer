9f1832f94b2111c90ed97a8f072296a18930167e
Guilherme Conte <gui.conte17@gmail.com>
2024-10-14 20:59:38 +0000

    Adjusting 'separarSalmoResposta' method

--- src/main/java/streamtimer/service/LiturgiaDiariaService.java
+++ src/main/java/streamtimer/service/LiturgiaDiariaService.java
@@ -1,6 +1,9 @@
 package streamtimer.service;
 
 import java.time.LocalDate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.springframework.web.client.RestTemplate;
 import streamtimer.model.LiturgiaCnbbApi;
 import streamtimer.model.LiturgiaDiaria;
@@ -72,34 +75,84 @@ public class LiturgiaDiariaService {
 
   private String separarSalmoResposta(String html) {
     try {
+      /**
+       * Para dias em que possuir mais de uma missa no dia, procura sempre a liturgia referente a missa do dia.
+       * começando a busca a partir do texto "Salmo Responsorial"
+       */
       String[] splitMissa = html.split("Missa do dia");
       String[] splitSalmo = null;
 
       if (splitMissa.length > 1) {
-        splitSalmo = splitMissa[1].split("Salmo responsorial")[1]
-            .split("\">R\\.( )*</font>")[1]
-            .split("(<br>)*( )*(\n)*( )*(<br>)*(<(/)*span.*)*<(/)*div.*")[0]
-            .split("</div")[0]
-            .split("</span>");
+        splitSalmo = splitMissa[1].split("Salmo responsorial");
       } else {
-        splitSalmo = html.split("Salmo responsorial")[1]
-            .split("\">R\\.( )*</font>")[1]
-            .split("(<br>)*( )*(\n)*( )*(<br>)*(<(/)*span.*)*<(/)*div.*")[0]
-            .split("<(/)*span.*")[0]
-            .split("<font.*");
+        splitSalmo = html.split("Salmo responsorial");
       }
 
-      return splitSalmo[0].replace("<br>", "")
-                          .replace("<div>", "")
-                          .replace("</div>", "")
-                          .replace("<p>", "")
-                          .replace("</p>", "")
-                          .replace("&nbsp;", "")
-                          .replace("  "," ")
-                          .replace("\n"," ")
-                          .replace("*","")
-                          .trim();
+      /**
+       * Interrompe a busca a partir do momento em que encontrar uma frase iniciada por um numero, e pode conter na sequencia uma letra,
+       * o que significa que está na primeira frase do salmo.
+       */
+      splitSalmo = splitSalmo[1].split("\\d+(\\w)*<");
+
+      /**
+       * Dado o trecho capturado que vai do titulo "Salmo responsorial" até a primeira frase do salmo,
+       * remove todas as tags HTML,
+       * remove as tabulações,
+       * remove espaços em branco,
+       * remove astericos "*"
+       */
+      String resposta = splitSalmo[0]
+              .replaceAll("<.*?>", " ")
+              .replace("\t", "")
+              .replace("&nbsp;", "")
+              .replace("*", "");
+
+      /**
+       * Aplica a seguinte expressao regular com o objetivo de procurar a resposta do salmo no trecho encontrado:
+       *
+       * @pattern (? i)R\.\s+((?:o|ó|Ó|a|á|Á|os|as|um|uma|uns|e|de|do|da|dos|das|ao|à|À|às|Às|com|por|em|no|na|nos|nas|se|que|é|É)?\s+(?!\d)\p{L}{2}(?![.-])\p{L}+.*?$)
+       *
+       * (?i)         - Ativa a flag "case-insensitive" (ignora diferenças entre maiúsculas e minúsculas).
+       * R\.          - Captura o literal "R.".
+       * \s+          - Captura um ou mais espaços após o "R.".
+       *
+       * ((?: ... )?) - Grupo não capturante e opcional, que captura as palavras com 2 ou 3 letras, listadas abaixo:
+       *  o|ó|Ó|a|á|Á|os|as|um|uma|uns|e|de|do|da|dos|das|ao|à|À|às|Às|com|por|em|no|na|nos|nas|se|que|é|É
+       *
+       * \s+          - Captura um ou mais espaços após o grupo de artigos/preposições/conjunções (se houver).
+       *
+       * (?!\d)       - Lookahead negativo: Garante que a palavra que segue não começará com um número.
+       * \p{L}{2}     - Captura uma palavra que começa com no mínimo duas letras alfabéticas (Unicode).
+       *
+       * (?![.-])     - Lookahead negativo: Garante que o terceiro caractere da palavra não seja um ponto (.) ou hífen (-).
+       *
+       * \p{L}+       - Captura o restante da palavra, que deve ser composta por letras alfabéticas.
+       * .*?          - Captura o restante da frase, até o final da linha.
+       *
+       * $            - Fim da linha, garantindo que a captura pare no final da linha mais próxima.
+       */
+      Matcher matcher = Pattern.compile("(?i)R\\.\\s+((?:o|ó|Ó|a|á|Á|os|as|um|uma|uns|e|de|do|da|dos|das|ao|à|À|às|Às|com|por|em|no|na|nos|nas|se|que|é|É)?\\s+(?!\\d)\\p{L}{2}(?![.-])\\p{L}+.*?$)", Pattern.DOTALL).matcher(resposta);
+
+      if (matcher.find()) {
+        /**
+         * Se encontrar o regex, achou a resposta e com isso:
+         * Verifica se no trecho da resposta existe mais de uma opção, separada com a palavra "Ou",
+         * se existir retorna a resposta principal.
+         * Remove o prefixo "R. " no inicio da resposta.
+         * Substitui espaços com mais de um caractere "  " por apenas um caractere " ".
+         */
+        resposta = matcher.group()
+                .split("\\b[oO]u:.*")[0]
+                .replaceAll("^R\\.\\s", "")
+                .replaceAll("\\s+", " ")
+                .trim();
+      } else {
+        resposta = "";
+      }
+
+      return resposta;
     } catch (Exception ex) {
+      System.out.println(ex);
       return "";
     }
   }
